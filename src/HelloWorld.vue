97d2d48aae54d6de28b6b6e1749b84e1f4971892
Truong <nguyenvantruong1372003@gmail.com>
2024-11-12 10:30:04 +0700

    Welcome OEworld

--- src/HelloWorld.vue
+++ src/HelloWorld.vue
@@ -1,7 +1,18 @@
 <template lang="pug">
 .scaffolding-sdk
+  //- <div :style="{ backgroundColor: bgColor }">
   h1 Hello Intern Team!
+  //- </div>
 </template>
 
 <script setup lang="ts">
+
+// const bgColor = ref('lightblue'); // Set your color here
 </script>
+
+<style scoped>
+.scaffolding-sdk {
+  background-color: rgb(238, 144, 144); /* Replace with any color */
+}
+</style>
+
