01df4470973f6fc18bdce1497de691898ee5813e
Truong123 <nguyenvantruong1372003@gmail.com>
2024-11-12 14:58:25 +0700

    View ComboProduct, RelatedItems component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template lang="pug">
 HelloWorld
+ComboProduct
+RelatedItems
 </template>
 
 <script setup lang="ts">
+import ComboProduct from './components/ComboProduct/ComboProduct.vue';
+import RelatedItems from './components/RelatedItems/RelatedItems.vue';
 import HelloWorld from './HelloWorld.vue';
 </script>
 
