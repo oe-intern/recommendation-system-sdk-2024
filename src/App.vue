1706463d810a2ba0bfe1e930bf7d5456a81da626
Truong123 <nguyenvantruong1372003@gmail.com>
2024-11-29 11:29:03 +0700

    Update: mount liquid, pass Id

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <template lang="pug">
-//- HelloWorld
 ComboProduct
-RelatedItems
 </template>
 
-<script setup lang="ts">
+<script setup>
 import ComboProduct from './components/ComboProduct/ComboProduct.vue';
 import RelatedItems from './components/RelatedItems/RelatedItems.vue';
-// import HelloWorld from './HelloWorld.vue';
+import { onMounted, createApp } from 'vue';
+import HelloWorld from './HelloWorld.vue';
+
+console.log("main.vue");
+onMounted(() => {
+  const embedEls = document.querySelectorAll('.fbt-embed');
+
+  console.log("Number of embed elements:", embedEls.length);
+  embedEls.forEach((el) => {
+    const productId = el.dataset.productId;
+    console.log("create App");
+    createApp(HelloWorld).mount(el);
+    createApp(ComboProduct, { id: productId }).mount(el);
+  });
+
+});
+
 </script>
 
 <style lang="scss">
 @use './scss/app.scss';
 
 body {
-  background-color: #f0f0f0; /* light background color */
+  background-color: #f0f0f0;
 }
 </style>
