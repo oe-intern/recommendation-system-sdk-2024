4ab38359faeb2ec869ba2ba6639e775adb48219c
Truong123 <nguyenvantruong1372003@gmail.com>
2024-12-20 18:04:11 +0700

    Add: use external component element

---
+++ src/components/Element/AddCart1.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+button.add-to-cart(
+  @click="clickCart",
+) 
+  | Add
+</template>
+<script setup lang="ts">
+const emit = defineEmits()
+function clickCart() {
+  emit('addToCart')
+}
+</script>
+<style lang="scss" scoped>
+@use '../../scss/components/element/add-cart1.scss'
+</style>
---
+++ src/components/Element/PlaceHolder.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+div.placeholder
+  | &nbsp;
+</template>
+
+<script setup lang="ts">
+defineProps({
+  height: {
+    type: String,
+  },
+  width: {
+    type: String,
+  },
+  borderRadius: {
+    type: String,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@use '../../scss/components/element/place-holder';
+.placeholder {
+  height: v-bind(height);
+  width: v-bind(width);
+  border-radius: v-bind(borderRadius);
+//   background: linear-gradient(45deg, #f3ec78, #af4261);
+}
+</style>
