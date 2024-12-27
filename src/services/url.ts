4693b05a115760db10133086ba10b7911b83895b
Truong123 <nguyenvantruong1372003@gmail.com>
2024-12-27 15:41:11 +0700

    Update: handle service error

--- src/services/url.ts
+++ src/services/url.ts
@@ -12,8 +12,12 @@ export function getHandleUrl(handle: string): string {
   return getStoreProductsUrl() + handle;
 }
 
-export function getDataUrl(handle: string): string {
+export function getProductJsonUrl(handle: string): string {
   return getHandleUrl(handle) + '.json';
+}
+
+export function getProductJsUrl(handle: string): string {
+  return getHandleUrl(handle) + '.js';
 }
 
 export function redirect (url : string) {
